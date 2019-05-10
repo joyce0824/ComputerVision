454c5ae91e53674d4af47e704f712dca54c6ccaf
郭欣怡 <joyce.guo@zhezhe.com.tw>
2019-05-10 15:23:02 +0800

    for test jenkins

--- app/src/main/java/com/example/g315/rgb2gray/MainActivity.java
+++ app/src/main/java/com/example/g315/rgb2gray/MainActivity.java
@@ -3,10 +3,9 @@ package com.example.g315.rgb2gray;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -27,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         imgIn = findViewById(R.id.img_input);
         imgOut = findViewById(R.id.img_output);
         imgSub = findViewById(R.id.img_sub);
+        //for text Jenkins
     }
 
     @Override
